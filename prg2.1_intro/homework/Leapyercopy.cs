8d581ad4cbf06cce81b3871bd19a9122d3fc902c
narumjungsnagin <joonst26@gmail.com>
2018-02-04 22:13:44 -0600

    made copy file.. it does not work in hw2 for some reason

---
+++ prg2.1_intro/homework/Leapyercopy.cs
@@ -0,0 +1,66 @@
+using System;
+
+namespace prg2._1_intro
+{
+    public class Leapyearcopy
+    {
+        
+        public void run()
+        {
+            Console.WriteLine("Print out year if u want");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if(year % 4 == 0 )
+            {
+                if(year % 100 == 0)
+                {         
+                    if(year % 400 == 0)
+                    {
+                        Console.WriteLine("It is leap year");
+
+                        Console.WriteLine("The next four leap year is...");
+
+                int nleap = year;
+                int cout = 0;
+                while(cout != 4)
+                { 
+                    nleap +=4;
+                    Console.WriteLine(nleap);
+                    cout++;
+                }
+
+                    } 
+
+                    else 
+                    {
+                        Console.WriteLine("It is NOT leap year");
+                        
+                    }                 
+            }
+            else 
+            {
+                Console.WriteLine("It is leap year");
+
+                Console.WriteLine("The next four leap year is...");
+
+                int nleap = year;
+                int cout = 0;
+                while(cout != 4)
+                { 
+                    nleap +=4;
+                    Console.WriteLine(nleap);
+                    cout++;
+                }
+                //good code
+            }
+
+         }         
+            if(year % 4 != 0)
+            {
+                Console.WriteLine("It is NOT leap year");
+                
+            }
+    }
+}
+}
+
