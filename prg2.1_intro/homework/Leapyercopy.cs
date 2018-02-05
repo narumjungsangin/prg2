64f7ec4b779f6556a42d01f41ae41bd2e3763c5a
narumjungsnagin <joonst26@gmail.com>
2018-02-04 22:18:39 -0600

    ...

--- prg2.1_intro/homework/Leapyercopy.cs
+++ prg2.1_intro/homework/Leapyercopy.cs
@@ -4,12 +4,10 @@ namespace prg2._1_intro
 {
     public class Leapyearcopy
     {
-        
         public void run()
         {
             Console.WriteLine("Print out year if u want");
             int year = Convert.ToInt32(Console.ReadLine());
-
             if(year % 4 == 0 )
             {
                 if(year % 100 == 0)
@@ -17,49 +15,33 @@ namespace prg2._1_intro
                     if(year % 400 == 0)
                     {
                         Console.WriteLine("It is leap year");
-
                         Console.WriteLine("The next four leap year is...");
-
-                int nleap = year;
-                int cout = 0;
-                while(cout != 4)
-                { 
-                    nleap +=4;
-                    Console.WriteLine(nleap);
-                    cout++;
-                }
-
+                        int nleap = year;
+                        int cout = 0;
+                        while(cout != 4)
+                            {    
+                            nleap +=4;
+                            Console.WriteLine(nleap);
+                            cout++;
+                            }
                     } 
-
-                    else 
-                    {
-                        Console.WriteLine("It is NOT leap year");
-                        
-                    }                 
+                    else Console.WriteLine("It is NOT leap year");                
             }
             else 
             {
                 Console.WriteLine("It is leap year");
-
                 Console.WriteLine("The next four leap year is...");
-
                 int nleap = year;
                 int cout = 0;
-                while(cout != 4)
-                { 
-                    nleap +=4;
-                    Console.WriteLine(nleap);
-                    cout++;
-                }
-                //good code
+                    while(cout != 4)
+                    { 
+                        nleap +=4;
+                        Console.WriteLine(nleap);
+                        cout++;
+                    }
             }
-
          }         
-            if(year % 4 != 0)
-            {
-                Console.WriteLine("It is NOT leap year");
-                
-            }
+            if(year % 4 != 0) Console.WriteLine("It is NOT leap year");           
     }
 }
 }
