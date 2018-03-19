a3154c96f7b9ac4df7b65310e125be2a5a4ad1a9
narumjungsnagin <joonst26@gmail.com>
2018-03-18 23:12:37 -0500

    wow

--- prg2.4_functional_programming/LinqPractice.cs
+++ prg2.4_functional_programming/LinqPractice.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace prg2._4_functional_programming
 {
@@ -18,21 +19,44 @@ namespace prg2._4_functional_programming
                     {
                         Console.WriteLine(word);
                     }
-
                 }
             }
-        
-        public static bool IsUpper(char c)
-        {
-            return (0x40 < c && 0x5b > c) ? true : false;
-        }
 
-       
-            
-        
+        public void ReadCSV()
+        {   
+            {
+                using(var reader = new StreamReader(@"C:\test.csv"))
+                {
+                    List<string> Student = new List<string>();
+                    List<int> Cyear = new List<int>();
+                    List<int> GPA = new List<int>();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+                        Student.Add(values[0]);
+                        Cyear.Add(Convert.ToInt32(values[1]));
+                        GPA.Add(Convert.ToInt32(values[2]));
+                    }
+               string ear = DateTime.Now.ToString("yyyy");
+               int numear = Convert.ToInt32(ear);
+               int[] ceararr = new int[Cyear.Count];
+                 for(int i = 0; i < Cyear.Count; i++)
+                {
+                    ceararr[i] = Cyear[i];
+                }
+                //class year arr 로 변환까지 함....
+               
+                
+                
+                }
+            }
+        }
 
         public void Problem2()
         {
+            ReadCSV();
+
 
         }
         public void Problem3()
